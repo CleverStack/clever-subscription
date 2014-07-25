a22e4b58cfa00799c43334c54c25687586b0e39c
Richard Gustin <richard.gustin86@gmail.com>
2014-07-25 21:34:02 +1000

    feat(account): Added Account into module

--- controllers/SubscriptionController.js
+++ controllers/SubscriptionController.js
@@ -12,5 +12,33 @@ module.exports = function( Controller, SubscriptionService, PermissionController
     /** @Prototype **/
     {
 
+        getAction: function() {
+            if ( this.req.query.AccountId !== undefined && this.req.query.AccountId != this.req.user.account.id ) {
+                return this.handleServiceMessage({ statuscode: 400, message: this.Class.service.model._name + " doesn't exist." })
+            }
+            this.req.query.AccountId = this.req.user.account.id;
+            this._super.apply( this, arguments );
+        },
+
+        postAction: function() {
+            this.req.body.AccountId = this.req.user.account.id;
+            this._super.apply( this, arguments );
+        },
+
+        putAction: function() {
+            if ( this.req.query.AccountId !== undefined && this.req.query.AccountId != this.req.user.account.id ) {
+                return this.handleServiceMessage({ statuscode: 400, message: this.Class.service.model._name + " doesn't exist." })
+            }
+            this.req.query.AccountId = this.req.user.account.id;
+            this._super.apply( this, arguments );
+        },
+
+        deleteAction: function() {
+            if ( this.req.query.AccountId !== undefined && this.req.query.AccountId != this.req.user.account.id ) {
+                return this.handleServiceMessage({ statuscode: 400, message: this.Class.service.model._name + " doesn't exist." })
+            }
+            this.req.query.AccountId = this.req.user.account.id;
+            this._super.apply( this, arguments );
+        }
     });
 }
